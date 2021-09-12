84488e8d1c2cfd791081b296dcbf8fbd363379b7
alucardmmc <amc573@gmail.com>
2021-09-12 03:11:46 -0500

    Update: serving files, class-based views and comments on posts

--- blog/models.py
+++ blog/models.py
@@ -15,7 +15,7 @@ class Tag(models.Model):
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    email_adress = models.EmailField()
+    email_address = models.EmailField()
 
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
@@ -27,10 +27,17 @@ class Author(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=150)
     excerpt = models.CharField(max_length=200)
-    image_name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='posts', null=True) 
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
     author = models.ForeignKey(
         Author, on_delete=models.SET_NULL, null=True, related_name='posts')
     tags = models.ManyToManyField(Tag)
+
+
+class Comment(models.Model):
+    user_name = models.CharField(max_length=120)
+    user_email = models.EmailField()
+    text = models.TextField(max_length=400)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
